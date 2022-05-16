e09c8e0098e3581396f830ad8189d97c096b4f13
Hila Hillel <hila2839@gmail.com>
2022-05-16 13:21:19 +0300

    stack h fixed

--- inc/stack.hpp
+++ inc/stack.hpp
@@ -33,13 +33,69 @@ public:
      * 
      */
     void print() const;
+
+    /**
+     * @brief push number to end of stack
+     * 
+     * @param a_number 
+     */
     void push(int a_number);
+
+
+    /**
+     * @brief pop out tha last number in stack
+     * 
+     * @return int 
+     */
     int pop();
+
+
+    /**
+     * @brief clear all items from stack
+     * 
+     */
     void clear();
+
+
+    /**
+     * @brief return the number from the end of stack
+     * 
+     * @return int& 
+     */
     int& top() const;
+
+
+    /**
+     * @brief Get the number of objects in stack
+     * 
+     * @return int 
+     */
     int get_size() const;
+
+
+    /**
+     * @brief add other stack to self stack
+     * 
+     * @param a_other 
+     */
     void operator+=(Stack a_other);
+
+
+    /**
+     * @brief add other stack to self stack and delete the other
+     * 
+     * @param a_other 
+     * @return self Stack 
+     */
     Stack& operator<<(Stack& a_other);
+
+
+    /**
+     * @brief  add self stack to other stack and delete the self
+     * 
+     * @param a_other 
+     * @return self Stack
+     */
     Stack& operator>>(Stack& a_other);
 
 
